bef425340e8dc811ea516d369d57502b8de3e7df
ItsOKayCZ <oldrich.krupauer@gmail.com>
2019-07-14 22:05:10 +0200

    Adding new code

--- 2019/Manager for home web server/Manager_for_home_web_server/mainwindow.cpp
+++ 2019/Manager for home web server/Manager_for_home_web_server/mainwindow.cpp
@@ -24,13 +24,17 @@ MainWindow::MainWindow(QWidget *parent) :
         // Showing the directories from the server
         QJsonArray arr = folderStructure.array();
         QStringList list;
+
+        list.append("Main");
         foreach(QJsonValue val, arr){
             if(val["type"] == "folder"){
                 list.append(val["name"].toString());
             }
         }
-        ui->directoryList->addItems(list);
 
+        ui->directoryList->clear();
+        ui->directoryContents->clear();
+        ui->directoryList->addItems(list);
     });
 
 
@@ -48,22 +52,22 @@ void MainWindow::on_addFiles_triggered()
 {
     QStringList filePath = QFileDialog::getOpenFileNames(this, "Select a file", "/home/oldrich/Desktop", "All files (*)");
 
-    QObject::connect(manager, &QNetworkAccessManager::finished, this, [=](QNetworkReply *reply){
-        if(reply->error()){
-            qDebug() << reply->errorString();
-            return;
-        }
-
-        qDebug() << reply->readAll();
-    });
-
     QStringList contents;
     foreach(QString path, filePath){
 
         QFile file(path);
         if(file.open(QIODevice::ReadOnly)){
+            QString fileName = file.fileName().section("/", -1, -1);
+
+            QString path;
+            if(itemPath == ""){
+                path = fileName;
+            } else {
+                path = itemPath + "/" + fileName;
+            }
+
             QString fileContent = file.readAll().toBase64();
-            request.setUrl(QUrl(url + "/uploadFile?path=" + itemPath + "&content=" + fileContent));
+            request.setUrl(QUrl(url + "/uploadFile?path=" + path + "&content=" + fileContent));
             manager->get(request);
         }
 
@@ -74,6 +78,9 @@ void MainWindow::on_addFiles_triggered()
 void MainWindow::on_directoryList_itemPressed(QListWidgetItem *item)
 {
     itemPath = item->text();
+    if(itemPath == "Main"){
+        itemPath = "";
+    }
 
     this->updateDirectoryContents(itemPath);
 
@@ -88,7 +95,7 @@ void MainWindow::updateDirectoryContents(QString directory){
     QStringList list;
 
     // Getting content for the root directory
-    if(path[0] == "Main"){
+    if(path[0] == ""){
 
         QJsonArray arr = folderStructure.array();
 
@@ -120,6 +127,7 @@ void MainWindow::updateDirectoryContents(QString directory){
 
                     if(pathIndex == path.size()){
 
+                        list.append("Go back");
                         foreach(QJsonValue val, contents){
                             if(val["type"] == "folder"){
                                 list.append(val["name"].toString() + "\t\t\t(Directory)");
@@ -145,8 +153,9 @@ void MainWindow::updateDirectoryContents(QString directory){
 
     if(list.size() > 0){
         ui->directoryContents->addItems(list);
+        ui->directoryLabel->setText("Directory: " + itemPath);
     } else {
-        QString msg = "No files in directory";
+        QString msg = "No files or folders in directory";
         ui->directoryContents->addItem(msg);
     }
 }
@@ -157,6 +166,13 @@ void MainWindow::on_directoryContents_itemClicked(QListWidgetItem *item)
 
     if(name.contains("Directory")){
         itemPath = itemPath + "/" + name.split("\t")[0];
+
+        updateDirectoryContents(itemPath);
+
+    } else if(name.contains("Go back")){
+        QStringList temp = itemPath.split("/");
+        temp.removeLast();
+        itemPath = temp.join("/");
 
         updateDirectoryContents(itemPath);
     }
